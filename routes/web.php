836f225e4895885ff16274807aa2bdfa2a316bd2
laythanL <laythanlateeshakamiharja@gmail.com>
2025-11-26 20:35:10 +0700

    user management controller

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::get('/', function () {
 
 Route::get("/login",[AuthController::class,"ShowLogin"])->name("login");
 Route::post("/login",[AuthController::class,"login"])->name("login.process");
-Route::get("/register",[AuthController::class,"showRegister"])->name("register");
-Route::post("/register",[AuthController::class,"Register"])->name("register.process"); 
+Route::get("/register",  [AuthController::class,"showRegister"])->name("register");
+Route::post("/register",[AuthController::class,"Register"])->name("register.process");
 Route::post("/logout",[AuthController::class,"Logout"])->name("logout");
 
 Route::middleware("auth:")->group(function(){
